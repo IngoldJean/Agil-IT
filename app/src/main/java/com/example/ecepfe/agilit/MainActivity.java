c7fe6755bd499caa4309e0aa0dd9d33259a1dbb6
IngoldJean <32537089+IngoldJean@users.noreply.github.com>
2017-10-24 17:00:58 +0200

    Action drag exemple
    Nous avons effectuer un premier drag d'image concluant et pu determiner un drop sur une certaine zone et y associer une action simple

--- app/src/main/java/com/example/ecepfe/agilit/MainActivity.java
+++ app/src/main/java/com/example/ecepfe/agilit/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.ecepfe.agilit;
 
+import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imagehelene;
+    TextView zonededrop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imagehelene = (ImageView) findViewById(R.id.helenepicture);
+        zonededrop = (TextView) findViewById(R.id.zonededrop);
+
+        imagehelene.setOnLongClickListener(longClickListener);
+        zonededrop.setOnDragListener(onDragListener);
     }
+
+    View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData clipData = ClipData.newPlainText("", "");
+            View.DragShadowBuilder dragShadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(clipData, dragShadowBuilder, v, 0);
+            return true;
+        }
+    };
+
+    View.OnDragListener onDragListener = new View.OnDragListener(){
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragevent=event.getAction();
+            switch (dragevent){
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    Log.i("drag lisener", "entered switch");
+                    break;
+                case DragEvent.ACTION_DROP:
+                    final View view = (View) event.getLocalState();
+                    zonededrop.setText("Laché dans zone");
+                    Log.i("drag lisener", "drop action");
+                    break;
+            }
+            return true;
+        }
+    };
 }
